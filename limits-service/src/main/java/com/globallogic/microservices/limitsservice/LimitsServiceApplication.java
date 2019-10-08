54e8c1bf36182509575e69f53c0c3dbe8e7ae7e7
Taher <taher@example.com>
2019-10-08 18:34:43 +0530

    Limit service

---
+++ limits-service/src/main/java/com/globallogic/microservices/limitsservice/LimitsServiceApplication.java
@@ -0,0 +1,15 @@
+package com.globallogic.microservices.limitsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = "com.globallogic.microservices")
+@SpringBootApplication
+public class LimitsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsServiceApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-config-server-service/src/test/java/com/globallogic/microservices/springcloudconfigserverservice/SpringCloudConfigServerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globallogic.microservices.springcloudconfigserverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
