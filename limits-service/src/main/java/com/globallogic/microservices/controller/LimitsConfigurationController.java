7963314d964d9451e9b672a187df884394a49f31
Taher <taher@example.com>
2019-10-13 18:58:17 +0530

    Hystrix

--- limits-service/src/main/java/com/globallogic/microservices/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/globallogic/microservices/controller/LimitsConfigurationController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.globallogic.microservices.bean.LimitConfiguration;
 import com.globallogic.microservices.configuration.Configuration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfigurationController {
@@ -16,6 +17,18 @@ public class LimitsConfigurationController {
 	@GetMapping("/limits")
 	public LimitConfiguration getLimitsFromConfiguration() {
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+
+	}
+
+	@GetMapping("/limits-fault-tolerance-example")
+	@HystrixCommand(fallbackMethod = "fallbackGetConfiguration")
+	public LimitConfiguration getConfiguration() {
+		throw new RuntimeException("Not Available");
+
+	}
+
+	public LimitConfiguration fallbackGetConfiguration() {
+		return new LimitConfiguration(111, 11);
 
 	}
 }
--- limits-service/src/main/java/com/globallogic/microservices/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/globallogic/microservices/limitsservice/LimitsServiceApplication.java
@@ -2,9 +2,11 @@ package com.globallogic.microservices.limitsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = "com.globallogic.microservices")
+@EnableHystrix
 @SpringBootApplication
 public class LimitsServiceApplication {
 
